74d33524f4de0c4c0aaec68d800ab3b2c2a45ae7
calgor <ahomayoni3@gmail.com>
2024-04-26 10:12:06 +0330

    feat: calculating TAT

--- gcra/leaky_bucket.lua
+++ gcra/leaky_bucket.lua
@@ -1,3 +1,18 @@
 local time_stamp  = tonumber(ARGV[1])
 local rps = tonumber(ARGV[2])
 local key = KEYS[1]
+
+local min = time_stamp -1
+redis.call('ZREMRANGEBYSCORE', key, '-inf', min)
+
+local last = redis.call('ZRANGE', key, -1, -1)
+
+local next = time_stamp
+
+if type(last) == 'table' and #last > 0 then
+  for key,value in pairs(last) do
+    next = tonumber(value) + 1/rps
+
+    break
+  end
+end
