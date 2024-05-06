48af21733e44ee710a9dd9a162ededf71b9288a2
calgor <ahomayoni3@gmail.com>
2024-05-06 16:49:36 +0330

    feat: added sliding window

---
+++ rate-limit/sliding_window.lua
@@ -0,0 +1,11 @@
+local current_time = redis.call('TIME')
+local trim_time = tonumber(current_time[1]) - @window
+redis.call('ZREMRANGEBYSCORE', @key, 0, trim_time)
+local request_count = redis.call('ZCARD',@key)
+
+if request_count < tonumber(@max_requests) then
+    redis.call('ZADD', @key, current_time[1], current_time[1] .. current_time[2])
+    redis.call('EXPIRE', @key, @window)
+    return 0
+end
+return 1
